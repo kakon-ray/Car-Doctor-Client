6f4f97f49261a8ddf0c9e628eaf6f179b270024f
Kakon Ray <thisiskakonray@gmail.com>
2022-04-27 18:57:57 +0600

    firebase api key add

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyB9_jp9qzqkGukeCfOl_9XNzvdFWjk6Rlo",
+  authDomain: "the-car-doctor-limited.firebaseapp.com",
+  projectId: "the-car-doctor-limited",
+  storageBucket: "the-car-doctor-limited.appspot.com",
+  messagingSenderId: "212271650845",
+  appId: "1:212271650845:web:eafed9a2d6cb351fb38ef4",
 };
 
 // Initialize Firebase
